10ce0f1f66436e6386ed15f588598ff2db12fcee
Pedro Hernandez Luqueño <1717110249@utectulancingo.edu.mx>
2019-03-28 11:20:16 -0600

    cambio

--- app/src/main/java/com/example/edpv110/MainActivity.java
+++ app/src/main/java/com/example/edpv110/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.edpv110;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -80,13 +81,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_login) {
+        if (id == R.id.nav_contacto) {
             // Handle the camera action
         } else if (id == R.id.nav_agregar) {
 
         } else if (id == R.id.nav_desaparecidas) {
+            Intent intent = new Intent(getApplicationContext(), desaparecidos.class);
+            startActivity(intent);
 
-        } else if (id == R.id.nav_contacto) {
+        } else if (id == R.id.nav_login) {
+            Intent intent = new Intent(getApplicationContext(), Login.class);
+            startActivity(intent);
 
         } else if (id == R.id.nav_share) {
 
---
+++ app/src/main/java/com/example/edpv110/desaparecidos.java
@@ -0,0 +1,13 @@
+package com.example.edpv110;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class desaparecidos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desaparecidos);
+    }
+}
